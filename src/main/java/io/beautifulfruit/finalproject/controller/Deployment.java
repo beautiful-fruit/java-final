15a0731fb7f54dca5d34962ac54634fcb67f5c32
Chisheng Chen <johnny1001s000602@gmail.com>
2025-04-15 23:13:20 +0800

    Add Deployment object
    This object provides some utilities of deployment. It now only
    contains a unimplemented function deploy, which will be able to
    deploy a container in the future.

---
+++ src/main/java/io/beautifulfruit/finalproject/controller/Deployment.java
@@ -0,0 +1,13 @@
+package io.beautifulfruit.finalproject.controller;
+
+public class Deployment {
+
+    /**
+     * Deploy a container and update the status of the container
+     *
+     * @param container the container
+     */
+    public static void deploy(Container container) {
+        // TODO: deploy the container and update the container status
+    }
+}
