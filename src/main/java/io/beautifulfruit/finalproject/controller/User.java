d417ab5b2c55f3a938f42de37265ef6a0240ed9d
Chisheng Chen <johnny1001s000602@gmail.com>
2025-04-15 22:54:04 +0800

    Implement function addContainer in User object
    This function adds a new container to the user.

--- src/main/java/io/beautifulfruit/finalproject/controller/User.java
+++ src/main/java/io/beautifulfruit/finalproject/controller/User.java
@@ -15,4 +15,18 @@ public class User {
         this.containers = new ArrayList<Container>();
     }
 
+    /**
+     * Add a new container to the User
+     *
+     * @param dockerCompose the docker compose plaintext file
+     *
+     * @return -1 if fail, otherwise 0
+     */
+    public int addContainer(String dockerCompose) {
+        Container container = new Container(dockerCompose);
+        if (container.status == Status.INVALID)
+            return -1;
+        containers.add(container);
+        return 0;
+    }
 }
