68c6ff3864ffe9f58bcb492b56f6236732d96220
Chisheng Chen <johnny1001s000602@gmail.com>
2025-04-25 15:03:28 +0800

    Add login authentication to Upload

--- src/main/java/io/beautifulfruit/finalproject/view/Upload.java
+++ src/main/java/io/beautifulfruit/finalproject/view/Upload.java
@@ -1,38 +1,66 @@
 package io.beautifulfruit.finalproject.view;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import io.beautifulfruit.finalproject.etcd.user.UserActiveModel;
+import io.beautifulfruit.finalproject.etcd.user.UserEntity;
+import jakarta.servlet.http.HttpServletRequest;
+
+
 
 @RestController
 public class Upload {
     String content;
     String uploadStatus = "No file";
-    User testUser = new User(1);
+
+    @Autowired
+    private UserEntity userEntity;
 
     @PostMapping("/upload")
-    public String handleUpload(@RequestBody Map<String, String> body, Model model) {
+    public String handleUpload(
+        @RequestBody Map<String, String> body,
+        Model model,
+        HttpServletRequest request
+        ){
+        String username = Validation.validateTokenFromCookie(request);
+
+        if (username == null)
+            return "fail";
+
+        UserActiveModel userActiveModel = userEntity.findUserByName(username).join();
+
+        if (userActiveModel == null)
+            return "fail";
+
         String file = body.get("file");
 
         if (file.length() == 0)
             return "fail";
-        testUser.addContainer(file);
-        DatabaseController.updateUser(testUser);
+
+        // TODO: add container
         uploadStatus = "Success";
         return "success";
     }
 
     @GetMapping("/upload_status")
-    public String getUploadStatus() {
-        User user = DatabaseController.queryUser(String.valueOf(testUser.id));
-        if (user == null)
+    public String getUploadStatus(HttpServletRequest request) {
+        String username = Validation.validateTokenFromCookie(request);
+
+        if (username == null)
             return "fail";
-        if (user.getContainerNumber() != 0)
-            return user.getContainer(user.getContainerNumber() - 1).dockerCompose;
+
+        UserActiveModel userActiveModel = userEntity.findUserByName(username).join();
+
+        if (userActiveModel == null)
+            return "fail";
+
+        // TODO: return container message
         return uploadStatus;
     }
 }
